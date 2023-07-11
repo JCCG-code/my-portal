710f959e64e0560131cab5a00979c17ff332be58
Juan Carlos Cuadrado Gracia <jccuadradogracia@gmail.com>
2023-07-11 13:20:54 +0200

    deploy-updated

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:JCCG-code/personal-portfolio.git master:gh-pages
+git push -f git@github.com:JCCG-code/my-portal.git master:gh-pages
 
 cd -

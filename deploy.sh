24943e76f4a51025b76ecb04b4849d46be719c88
Juan Carlos Cuadrado Gracia <jccuadradogracia@gmail.com>
2023-07-12 12:35:19 +0200

    aca

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://JCCG-code.github.io
-git push -f git@github.com:JCCG-code/JCCG-code.github.io.git main
+# git push -f git@github.com:JCCG-code/JCCG-code.github.io.git master
 
-# if you are deploying to https://JCCG-code.github.io/<REPO>
-# git push -f git@github.com:JCCG-code/<REPO>.git main:gh-pages
+# if you are deploying to https://JCCG-code.github.io/my-portal
+git push -f git@github.com:JCCG-code/my-portal.git master:gh-pages
 
 cd -
